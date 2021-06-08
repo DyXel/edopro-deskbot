e2779380d4b70a7909a118c1db8c8b8b7f4ee676
Dylam De La Torre <dyxel04@gmail.com>
2021-06-08 21:26:59 +0200

    init repo and add stub compilation plus other stuff

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021, Dylam De La Torre <dyxel04@gmail.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
+{
+	return 0;
+}
