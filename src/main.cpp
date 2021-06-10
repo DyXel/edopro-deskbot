1ef298c15c94597e992ae415686b2b6f3e95ae8a
Dylam De La Torre <dyxel04@gmail.com>
2021-06-11 01:57:01 +0200

    make executable actually connect and initialize client

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,31 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+#include <boost/asio/connect.hpp>
+#include <iostream>
+#include <string_view>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
+#include "client.hpp"
+
+auto main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) -> int
 {
+	auto const host = std::string_view("localhost");
+	auto const port = std::string_view("7911");
+	boost::asio::io_context io_context;
+	std::optional<Client> client;
+	try
+	{
+		boost::asio::ip::tcp::resolver resolver(io_context);
+		auto endpoints = resolver.resolve(host, port);
+		boost::asio::ip::tcp::socket socket(io_context);
+		boost::asio::connect(socket, endpoints);
+		client.emplace(std::move(socket), Firebot::Core::Options{});
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << "Error while initializing client: " << e.what() << "\n";
+		return 1;
+	}
+	io_context.run();
 	return 0;
 }
