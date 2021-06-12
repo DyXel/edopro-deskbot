53ecba8b457b77011666196b5d94428dacb8c75c
Dylam De La Torre <dyxel04@gmail.com>
2021-06-13 01:42:13 +0200

    add EncodeContext
    required to parse and encode messages to the protobuf format, which are
    then passed to the AI core and analyzed.
    
    Encapsulating the encode mechanism within the front-end is still
    preferable than inyecting the encoder code into the AI, which should
    have no knowledge about ygopro core messages.

---
+++ src/encode_context.cpp
@@ -0,0 +1,58 @@
+#include "encode_context.hpp"
+
+EncodeContext::EncodeContext() noexcept
+	: match_win_reason_(0)
+{}
+
+EncodeContext::~EncodeContext() noexcept = default;
+
+auto EncodeContext::pile_size(Con con, Loc loc) const noexcept -> size_t
+{
+	return board_.frame().pile(con, loc).size();
+}
+
+auto EncodeContext::get_match_win_reason() const noexcept -> uint32_t
+{
+	return match_win_reason_;
+}
+
+auto EncodeContext::has_xyz_mat(Place const& p) const noexcept -> bool
+{
+	return !board_.frame().zone(p).materials.empty();
+}
+
+auto EncodeContext::get_xyz_left(Place const& left) const noexcept -> Place
+{
+	return left_.find(left)->second;
+}
+
+auto EncodeContext::match_win_reason(uint32_t reason) noexcept -> void
+{
+	match_win_reason_ = reason;
+}
+
+auto EncodeContext::xyz_mat_defer(Place const& place) noexcept -> void
+{
+	deferred_.emplace_back(place);
+}
+
+auto EncodeContext::take_deferred_xyz_mat() noexcept -> std::vector<Place>
+{
+	decltype(deferred_) taken{};
+	std::swap(taken, deferred_);
+	return taken;
+}
+
+auto EncodeContext::xyz_left(Place const& left, Place const& from) noexcept
+	-> void
+{
+	left_[left] = from;
+}
+
+auto EncodeContext::parse(YGOpen::Proto::Duel::Msg const& msg) noexcept -> void
+{
+	if(msg.t_case() == YGOpen::Proto::Duel::Msg::kEvent)
+		parse_event(board_, msg.event());
+	for(auto const& query : msg.queries())
+		parse_query(board_.frame(), query);
+}
---
+++ src/encode_context.hpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2021, Dylam De La Torre <dyxel04@gmail.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+#ifndef EDOPRO_FIREBOT_ENCODE_CONTEXT_HPP
+#define EDOPRO_FIREBOT_ENCODE_CONTEXT_HPP
+#include <google/protobuf/arena.h>
+
+#include <ygopen/client/board.hpp>
+#include <ygopen/client/card.hpp>
+#include <ygopen/client/frame.hpp>
+#include <ygopen/client/parse_event.hpp>
+#include <ygopen/client/parse_query.hpp>
+#include <ygopen/client/value_wrappers.hpp>
+#include <ygopen/codec/encode_common.hpp>
+
+class EncodeContext final : public YGOpen::Codec::IEncodeContext
+{
+public:
+	EncodeContext() noexcept;
+	~EncodeContext() noexcept;
+
+	auto pile_size(Con con, Loc loc) const noexcept -> size_t override;
+	auto get_match_win_reason() const noexcept -> uint32_t override;
+	auto has_xyz_mat(Place const& p) const noexcept -> bool override;
+	auto get_xyz_left(Place const& left) const noexcept -> Place override;
+	auto match_win_reason(uint32_t reason) noexcept -> void override;
+	auto xyz_mat_defer(Place const& place) noexcept -> void override;
+	auto take_deferred_xyz_mat() noexcept -> std::vector<Place> override;
+	auto xyz_left(Place const& left, Place const& from) noexcept
+		-> void override;
+
+	auto parse(YGOpen::Proto::Duel::Msg const& msg) noexcept -> void;
+
+private:
+	template<typename T>
+	using WrapperType = YGOpen::Client::Value<T>;
+	using CardType = YGOpen::Client::BasicCard<WrapperType>;
+	using FrameType = YGOpen::Client::BasicFrame<CardType>;
+	using BoardType = YGOpen::Client::BasicBoard<FrameType, WrapperType>;
+	BoardType board_;
+
+	// Encoder context data.
+	uint32_t match_win_reason_;
+	std::map<Place, Place, YGOpen::Proto::Duel::PlaceLess> left_;
+	std::vector<Place> deferred_;
+};
+
+#endif // EDOPRO_FIREBOT_ENCODE_CONTEXT_HPP
