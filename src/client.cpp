324b2c81fff0d6b593a555bfe3edfc9eecefe57f
Dylam De La Torre <dyxel04@gmail.com>
2022-06-18 18:43:55 +0200

    update used banlist

--- src/client.cpp
+++ src/client.cpp
@@ -47,7 +47,7 @@ Client::Client(boost::asio::ip::tcp::socket socket, Options const& options)
 		static constexpr uint64_t DUEL_FLAGS = 4295157760U;
 		auto create_game = YGOPro::CTOSMsg::CreateGame{};
 		auto& hi = create_game.host_info;
-		hi.banlist_hash = 2512293045U; // "2022.02 TCG"
+		hi.banlist_hash = 186804830U; // "2022.5 TCG"
 		hi.allowed = 0x3U; // "OCG/TCG"
 		hi.starting_draw_count = 5U;
 		hi.draw_count_per_turn = 1U;
